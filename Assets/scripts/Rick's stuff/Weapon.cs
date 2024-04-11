50f3f7b3e2caad836087e8473d21c615b77f28ed
RiftVoid <159392203+RiftVoid@users.noreply.github.com>
2024-04-11 13:06:37 +1000

    pain

---
+++ Assets/scripts/Rick's stuff/Weapon.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    //mouse inputs (x,y,click)
+    //need to take into account of charter direction 
+    //need contact info ( can outsouce it to the emeny)
+    //if want to turn into a combat moduel need state change
+
+    enum WeaponState
+    {
+        Sword,
+        Bow,
+
+    }
+
+    [SerializeField] private WeaponState state;
+    [SerializeField] private GameObject sword;
+    [SerializeField] private GameObject actualSword;
+    [SerializeField] private GameObject bow;
+    [SerializeField] private GameObject arrowPrefab;
+
+    [SerializeField] private float swordSpeed;
+    [SerializeField] private float swordTime;
+    [SerializeField] private float swordTimer;
+    [SerializeField] private bool swordStabing;
+
+    [SerializeField] private bool bowDelay;
+
+    private GameObject currentArrow;
+
+    private Vector2 vectorSword;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = WeaponState.Sword;
+
+
+        //HOW to arrow
+        //currentArrow = Instantiate(arrowPrefab, transform.position, Quaternion.identity);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Sets sword or bow active
+        if (state == WeaponState.Sword)
+        {
+            sword.SetActive(true);
+            bow.SetActive(false);
+
+        }
+        else if (state == WeaponState.Bow)
+        {
+            sword.SetActive(false) ;
+            bow.SetActive(true) ;
+        }
+
+
+        //changes weaponstate
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if(state == WeaponState.Sword)
+            {
+                state = WeaponState.Bow;
+            }
+            else if (state == WeaponState.Bow)
+            {
+                state = WeaponState.Sword;
+            }
+        }
+
+
+        Vector3 position = Input.mousePosition;
+        position.z = 5;
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(position);
+
+        sword.transform.position = worldPos;    
+
+        Debug.Log(Input.mousePosition);
+
+        vectorSword = (Vector2)transform.position - worldPos;
+        if (state == WeaponState.Sword)
+        {
+
+            sword.transform.right = vectorSword;
+            //sword.transform.rotation = Quaternion.LookRotation(vectorSword);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(state == WeaponState.Sword && swordStabing == false)
+            {
+                //actualSword.transform.right = actualSword.transform.right + (swordSpeed * Time.deltaTime); 
+                swordStabing = true;   
+                
+              
+            }
+            else if (state == WeaponState.Bow && bowDelay == false)
+            {
+                currentArrow = Instantiate(arrowPrefab, transform.position, Quaternion.identity);
+                bowDelay = true;
+            }
+        }
+
+        if (swordStabing == true)
+        {
+            float stabSpeed = swordSpeed * Time.deltaTime;
+            swordTimer += Time.deltaTime;
+            if (swordTimer < swordTime / 2)
+            {
+                actualSword.transform.position += Vector3.right * stabSpeed;
+            }
+            else if (swordTimer > swordTime)
+            {
+                swordStabing = false;
+                swordTimer = 0;
+                actualSword.transform.position = Vector3.zero;
+            }
+            else
+            {
+                actualSword.transform.position -= Vector3.right * stabSpeed;
+
+            }
+        }
+
+        if (currentArrow == null) 
+        {
+
+            bowDelay = false;
+        }
+    }
+
+}
